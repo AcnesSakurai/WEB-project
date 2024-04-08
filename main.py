a9e3673e695e33d7c4cefb60dac1a3922aee1f2e
Виктория <vikavip06052006@gmail.com>
2024-04-08 17:19:14 +0300

    Запись информационного блока во всплывающие уведомления для команды /help

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ from telegram import ReplyKeyboardMarkup
 
 
 BOT_TOKEN = "6316601466:AAGR0OJks7WwNgvbr6gj2uJzHBU47SH0JK0"
-reply_keyboard = [['/start', '/date', "/set"],
-                  ['/time', '/help', "/unset"]]
+reply_keyboard = [['/start', '/help', "/info"]]
 
 markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
 
@@ -20,7 +19,7 @@ TIMER = 5
 
 
 async def start(update, context):
-    img = open('card.png', 'rb')
+    img = open('card.jpg', 'rb')
     await context.bot.send_photo(
         update.message.chat_id,
         img,
@@ -30,15 +29,25 @@ async def start(update, context):
 
 async def help(update, context):
     await update.message.reply_text(
-        "Я бот справочник.")
+        "Привет! Я готова тебе помочь с любым возникшим вопросом. Вот что я умею:"
+        "1. /start - запускает бота-таролога,"
+        "2. /help - выводит список доступных команд,"
+        "3. /info - выводит дополнительную информацию о боте-тарологе и дополнительную информацию о гаданиях и значениях карт"
+        "")
+
+
+async def info(update, context):
+    await update.message.reply_text(
+        "Наш бот не идиален, если Вас заинтересовало гадание, то можете узнать подробнее перейдя по ссылке https://gadalkindom.ru/gadanie/taro/znachenie-kart-taro ")
 
 
 def main():
     application = Application.builder().token(BOT_TOKEN).build()
-    text_handler = MessageHandler(filters.TEXT, start)
+    text_handler = MessageHandler(filters.TEXT & ~filters.COMMAND, start)
     application.add_handler(text_handler)
     application.add_handler(CommandHandler("help", help))
     application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("info", info))
     application.run_polling()
 
 
